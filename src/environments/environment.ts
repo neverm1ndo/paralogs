0353dc8137bb4a886807429f71e987a0bb1e2eba
neverm1ndo <reddigitalsw@gmail.com>
2019-08-21 16:21:26 +0300

    envronnents

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
+    apiKey: "AIzaSyCvQ_C1t70QmrJzCEN3beqUiGyhxH14Ukg",
+    authDomain: "paralogs-1b6d8.firebaseapp.com",
+    databaseURL: "https://paralogs-1b6d8.firebaseio.com",
+    projectId: "paralogs-1b6d8",
+    storageBucket: "paralogs-1b6d8.appspot.com",
+    messagingSenderId: "1017597223825",
+    appId: "1:1017597223825:web:0e3cecc3198d2d9d"
   }
 };
 
